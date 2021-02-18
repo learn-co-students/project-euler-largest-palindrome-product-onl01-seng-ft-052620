5a2afaf931981d735951eccd62928b328670ec5a
aaronfeingold <ajfeingold88@gmail.com>
2021-02-17 23:02:57 -0600

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,39 @@
+require 'pry'
 # Implement your procedural solution here!
+# what is a palindrome?
+# is there meta code for that already?
+# ignore that...
+# what are all the three digit numbers?
+# numbers 100 to 999 inclusive
+# 
+
+# evlaute if integer is palindrome
+def is_palindrome?(input)
+  # where a stringified num can be compared to itself fowawrds and reverse
+  # same both ways is true  
+  input.to_s == input.to_s.reverse
+   ## REFACTOR THIS MONSTRONSITY 
+end
+
+def largest_palindrome_product
+  #need to save the largest to variable
+  #largest can be reset depending on the loop
+  #do some 100..999 and for each one another loop 100..999 (N)^2
+  largest = 0
+  #for every 3 digit num multiply it by any other three digit number 
+  (100..999).each do |x|
+    # this seems slow for this question
+    (100..999).each do |y|
+      product = x * y
+      if (is_palindrome?(product) && product > largest)
+        # pass? largest is now the product till done exectuing maths
+        largest = product
+      end
+    end
+  end
+
+  return largest
+end
+
+# print is_palindrome?(909)
+# print largest_palindrome_product
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,35 @@
+require 'pry'
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  attr_reader :answer
+
+  def initialize
+    @answer = largest_palindrome_product
+  end
+
+  def is_palindrome?(product)
+    product.to_s == product.to_s.reverse
+  end
+
+  def largest_palindrome_product
+    largest = 0
+  
+      (100..999).each do |x|
+        (100..999).each do |y|
+          product = x * y
+          if (is_palindrome?(product) && product > largest)
+            # what is self? a new object <LargestPalindromeProduct:0x00007fc3b79ada80>
+            # x and y are 101 and 101, product os 10201
+            # do we need to be able to call this info anywhere else?
+            # can we make accessible these variables via instance methods?
+            # binding.pry
+            largest = product
+          end
+        end
+      end
+    largest
+  end
+
+end
+
+print LargestPalindromeProduct.new.answer
